27b71710c97e34f4f7625ac961eaf680a37d7df9
DuncantheeDuncan <mthembup69@gmail.com>
2019-07-18 01:51:33 +0200

    --

--- src/soccer/League.java
+++ src/soccer/League.java
@@ -4,23 +4,23 @@ public class League {
 
     public static void main(String[] args) {
 
-        Player player1  = new Player();
-        player1.playerName ="George Elliot";
+        Player player1 = new Player();
+        player1.playerName = "George Elliot";
 
-        Player player2  = new Player();
+        Player player2 = new Player();
         player2.playerName = "Graham Green";
 
-        Player player3  = new Player();
+        Player player3 = new Player();
         player3.playerName = "Geoffrey Chaucer";
 
-        Player[] thePlayers = {player1,player2,player3};
+        Player[] thePlayers = {player1, player2, player3};
         Team team1 = new Team();
         team1.teamName = "The Greens";
         team1.playerArray = thePlayers;
 
 //        player1.playerName ="Robert Service";
         Team team2 = new Team();
-        team2.teamName ="The Reds";
+        team2.teamName = "The Reds";
         team2.playerArray = new Player[3];
         team2.playerArray[0] = new Player();
         team2.playerArray[0].playerName = "Robert Burns";
@@ -46,12 +46,32 @@ public class League {
 
 
         System.out.println("Goal scored after " +
-             currentGame.goals[0].theTime + " mins by " +
-             currentGame.goals[0].thePlayer.playerName + " of " +
-             currentGame.goals[0].theTeam.teamName);
+                currentGame.goals[0].theTime + " mins by " +
+                currentGame.goals[0].thePlayer.playerName + " of " +
+                currentGame.goals[0].theTeam.teamName);
 
 
-        /*this for loop iterate the playerArray */
+        for (Player thePlayer : team2.playerArray) {
+//            if (thePlayer.playerName.matches(".*mla.*")) {// .*string.* searches and print the whole String..
+            StringBuilder familyNameFirst = new StringBuilder();
+//                System.out.println("Found " + thePlayer.playerName);
+                System.out.println("Last Name is: "+thePlayer.playerName.split(" ")[1]);
+                String name[] =thePlayer.playerName.split(" ");
+
+
+
+                familyNameFirst.append(name[1]);
+                familyNameFirst.append(", ");
+                familyNameFirst.append(name[0]);
+                System.out.println(familyNameFirst);
+                familyNameFirst.delete(0, familyNameFirst.length());// this does not seem to work..
+
+
+//            }
+
+
+
+            /*this for loop iterate the playerArray */
 //
 //        for (Player thePlayer: team1.playerArray){
 //
@@ -62,5 +82,6 @@ public class League {
 //
 //            System.out.println(thePlayer.playerName);
 //        }
+        }
     }
 }
